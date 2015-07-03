e6ecad33dd873c9e3f2e1f68711940771a21f940
tejak <teja.kondapalli@yahoo.com>
2015-07-03 13:32:08 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function() {
+  
+   /* code for button 1  -- Turn the light Red */
+  
+   /* code for button 2 --- Say STOP */
+   $('#button2').click(
+     function() {
+         $('#top-light').html("<p> STOP </p>");
+     }
+   );
+  
+   /* code for button 3 -- Turn the light Yellow */
+  
+   /* code for button 4 -- Turn the light Green */
+  
+   /* code for button 5 -- Say GO! */
+
+   /* code for button 6 -- Make Green Light Blink -- Extension!! */
+  
+});
+                  
+                  
+                  
